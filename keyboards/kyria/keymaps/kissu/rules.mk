5e2722cdd2d46632437b5ac3783cbae68e4621ec
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-05-04 02:30:06 +0200

    modifs

---
+++ keyboards/kyria/keymaps/kissu/rules.mk
@@ -0,0 +1,6 @@
+# OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enables the Leader shortcut funtionality
+AUTO_SHIFT_ENABLE = yes
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -1,5 +1,6 @@
 # generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
-
+MCU = atmega32u4
+BOOTLOADER = atmel-dfu
 MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
 LTO_ENABLE = yes
