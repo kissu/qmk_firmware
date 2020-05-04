bc114e844c2c4efccfca3a6e65420a13ae97c3b6
Konstantin BIFERT <konstantin.bifert@gmail.com>
2020-05-04 03:33:33 +0200

    working

--- keyboards/kyria/keymaps/kissu/rules.mk
+++ keyboards/kyria/keymaps/kissu/rules.mk
@@ -1,6 +1,7 @@
-# OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-LEADER_ENABLE = yes        # Enables the Leader shortcut funtionality
-AUTO_SHIFT_ENABLE = yes
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = no        # Enable the Leader Key feature
+MOUSEKEY_ENABLE = no
+
+AUTO_SHIFT_ENABLE = yes
